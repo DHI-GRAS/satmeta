f857bd0b7e79a81239788e52af4da8206171a9d7
Jonas <josl@dhi-gras.com>
2017-03-16 21:44:07 +0100

    add generic _get_value function

--- sentinel_meta/converters.py
+++ sentinel_meta/converters.py
@@ -6,39 +6,39 @@ import dateutil.parser
 logger = logging.getLogger(__name__)
 
 
-def get_single(root, tagname, attrname=None):
+def _get_value(element, attrname=None, to_type=None):
+    if attrname is not None:
+        s = element.attrib[attrname]
+    else:
+        s = element.text
+    if to_type is None:
+        return s
+    else:
+        return to_type(s)
+
+
+def get_single(root, tagname, attrname=None, to_type=None):
     results = root.findall('.//{}'.format(tagname), namespaces=root.nsmap)
     if len(results) != 1:
         raise ValueError('Expected to find a single instance of tag \'{}\'. Found {}.'.format(tagname, len(results)))
-    if attrname is not None:
-        return results[0].attrib[attrname]
-    else:
-        return results[0].text
+    return _get_value(results[0], attrname=attrname, to_type=to_type)
 
 
-def get_instance(root, tagname, attrname=None, index=0):
+def get_instance(root, tagname, attrname=None, index=0, to_type=None):
     result = root.findall('.//{}'.format(tagname), namespaces=root.nsmap)[index]
-    if attrname is not None:
-        return result.attrib[attrname]
-    else:
-        return result.text
+    return _get_value(result, attrname=attrname, to_type=to_type)
 
 
-def get_all(root, tagname, attrname=None):
+def get_all(root, tagname, attrname=None, to_type=None):
     results = root.findall('.//{}'.format(tagname), namespaces=root.nsmap)
-    if attrname is not None:
-        return [e.attrib[attrname] for e in results]
-    else:
-        return [e.text for e in results]
+    return [_get_value(e, attrname=attrname, to_type=to_type) for e in results]
 
 
 def get_single_date(root, tagname):
-    datestr = get_single(root, tagname)
-    return dateutil.parser.parse(datestr)
+    return get_single(root, tagname, to_type=dateutil.parser.parse)
 
 
 def _parse_coordinates_str(cs):
-    """Parse coordinates string"""
     return [tuple(map(float, s.split(','))) for s in cs.split()]
 
 
@@ -46,7 +46,11 @@ def _coords_to_polygon(coords):
     return shapely.geometry.shape(dict(type='Polygon', coordinates=[coords]))
 
 
-def get_single_polygon(root, tagname):
-    coordinates_str = get_single(root, tagname)
+def parse_coords(coordinates_str):
+    """Parse GeoJSON-like coordinates string as a Polygon"""
     coords = _parse_coordinates_str(coordinates_str)
-    return shapely.geometry.shape(dict(type='Polygon', coordinates=[coords]))
+    return _coords_to_polygon(coords)
+
+
+def get_single_polygon(root, tagname):
+    return get_single(root, tagname, to_type=parse_coords)
