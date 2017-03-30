2d1cdf87e79b6e33acf92ccea9ef0df9532a84a9
Jonas <josl@dhi-gras.com>
2017-03-30 10:22:45 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentinel_meta',
-    version='0.1',
+    version='0.2',
     description='Sentinel Meta Data Extraction',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
