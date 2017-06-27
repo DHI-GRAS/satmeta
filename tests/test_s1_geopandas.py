bd91151a18f1c64dbc6e01d05a3ea75c1e241434
Jonas <josl@dhi-gras.com>
2017-06-27 15:20:39 +0200

    really do skip if no geopandas

--- tests/test_s1_geopandas.py
+++ tests/test_s1_geopandas.py
@@ -1,10 +1,14 @@
 import pytest
 
+from .data.s1 import test_data
+
+pexpect = pytest.importorskip('geopandas')
+
 try:
     import geopandas as gpd
-    from satmeta.s1 import to_geopandas
+    _no_geopandas = False
 except ImportError:
-    pytestmark = pytest.mark.skip(reason='No geopandas')
+    _no_geopandas = True
 
 try:
     import joblib
@@ -12,17 +16,19 @@ try:
 except ImportError:
     _no_joblib = True
 
-from .data.s1 import test_data
 
-
+@pytest.mark.skipif(_no_geopandas, reason='No geopandas')
 def test_meta_as_geopandas():
+    from satmeta.s1 import to_geopandas
     safes = [test_data['SAFE'], test_data['zip']]
     gdf = to_geopandas.meta_as_geopandas(safes)
     assert isinstance(gdf, gpd.GeoDataFrame)
 
 
+@pytest.mark.skipif(_no_geopandas, reason='No geopandas')
 @pytest.mark.skipif(_no_joblib, reason='No joblib')
 def test_meta_as_geopandas_parallel():
+    from satmeta.s1 import to_geopandas
     safes = [test_data['SAFE'], test_data['zip']]
     gdf = to_geopandas.meta_as_geopandas_parallel(safes)
     assert isinstance(gdf, gpd.GeoDataFrame)
