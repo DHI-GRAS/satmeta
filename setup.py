f7fcb698be19655d561e9f241434b577b676d0e4
Jonas <josl@dhi-gras.com>
2017-08-04 22:18:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='satmeta',
-    version='0.9',
+    version='0.9.1',
     description='Satellite Meta Data Extraction',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
