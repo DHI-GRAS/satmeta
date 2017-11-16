4c5cf44dafae08b91114c577793654f55e2fa850
Jonas Sølvsteen <j08lue@gmail.com>
2017-11-16 13:57:41 +0100

    add more tests

--- tests/s1/test_geopandas.py
+++ tests/s1/test_geopandas.py
@@ -1,4 +1,11 @@
+import datetime
+
 import pytest
+
+try:
+    import satmeta.s1.to_geopandas as s1geopandas
+except ImportError:
+    pytestmark = pytest.mark.skip(reason='geopandas not installed')
 
 from .data import test_data
 
@@ -6,14 +13,33 @@ gpd = pytest.importorskip('geopandas')
 
 
 def test_meta_as_geopandas():
-    from satmeta.s1 import to_geopandas
-    safes = [test_data['SAFE'], test_data['zip']]
-    gdf = to_geopandas.meta_as_geopandas(safes)
+    infiles = [test_data['SAFE'], test_data['zip']]
+    gdf = s1geopandas.meta_as_geopandas(infiles)
     assert isinstance(gdf, gpd.GeoDataFrame)
 
 
-def test_meta_as_geopandas_parallel():
-    from satmeta.s1 import to_geopandas
-    safes = [test_data['SAFE'], test_data['zip']]
-    gdf = to_geopandas.meta_as_geopandas_parallel(safes)
+def test_meta_as_geopandas_multiprocessing():
+    infiles = [test_data['SAFE'], test_data['zip']]
+    gdf = s1geopandas.meta_as_geopandas(infiles, multiprocessing_above=0)
     assert isinstance(gdf, gpd.GeoDataFrame)
+
+
+def test_group_gdf():
+    infiles = [test_data['SAFE'], test_data['zip']]
+    gdf = s1geopandas.meta_as_geopandas(infiles)
+    grouped = s1geopandas.group_gdf(gdf)
+    for meta, infiles_group in grouped:
+        assert isinstance(meta, dict)
+        break
+
+
+def test_filter_gdf():
+    infiles = [test_data['SAFE'], test_data['zip']]
+    gdf = s1geopandas.meta_as_geopandas(infiles)
+    gdf_filtered = s1geopandas.filter_gdf(
+        gdf,
+        rel_orbit_numbers=gdf['relative_orbit_number'].values,
+        footprint_overlaps=gdf['footprint'].values[0],
+        start_date=datetime.datetime(2016, 1, 1),
+        end_date=datetime.datetime(2016, 12, 31))
+    assert len(gdf) == len(gdf_filtered)
