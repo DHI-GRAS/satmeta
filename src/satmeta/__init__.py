539cc94cc5c1f877ce31c078105cb25412fd2cd5
Jonas Sølvsteen <j08lue@gmail.com>
2018-02-06 13:37:46 +0100

    versioneer

--- src/satmeta/__init__.py
+++ src/satmeta/__init__.py
@@ -5,3 +5,7 @@ from satmeta import l8
 # keys present in all metadata dictionaries
 COMMON_KEYS = [
     'sensing_time', 'title', 'spacecraft']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
